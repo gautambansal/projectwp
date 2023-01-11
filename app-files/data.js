3491c4554122fd01e6ea33974273a62dd4c325f9
bansal_g <gautam.bansal@Xecurify.com>
2023-01-11 13:38:48 +0530

    added

---
+++ app-files/data.js
@@ -0,0 +1,126 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-gdhjk",
+      "name": "gdhjk",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4075.5,
+      "initialViewParameters": {
+        "yaw": 0.4770898800829322,
+        "pitch": 0.04987687854011469,
+        "fov": 1.2463883365493036
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8314883875701753,
+          "pitch": -1.4843069447428068,
+          "rotation": 0,
+          "target": "0-gdhjk"
+        },
+        {
+          "yaw": 2.9586449237794374,
+          "pitch": 1.5707963267948966,
+          "rotation": 0,
+          "target": "0-gdhjk"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.9811028700319344,
+          "pitch": 0.28714814845769077,
+          "title": "Soham",
+          "text": "BA"
+        },
+        {
+          "yaw": -0.15044386549172728,
+          "pitch": 0.20515866444706177,
+          "title": "Vikas",
+          "text": "SE"
+        },
+        {
+          "yaw": -0.3286390020285239,
+          "pitch": 0.3677629462956915,
+          "title": "Nilesh",
+          "text": "SE"
+        },
+        {
+          "yaw": 0.3953485115673132,
+          "pitch": 0.14464101229085813,
+          "title": "302 Entrance",
+          "text": "Text"
+        },
+        {
+          "yaw": 1.5430515468035981,
+          "pitch": 0.07532352809960408,
+          "title": "Shruti",
+          "text": "WCD"
+        },
+        {
+          "yaw": 1.7694027661519476,
+          "pitch": 0.23770445515548744,
+          "title": "Nagesh",
+          "text": "WCD"
+        },
+        {
+          "yaw": 2.550525260287773,
+          "pitch": 0.22555912925249721,
+          "title": "Jayesh",
+          "text": "SE"
+        },
+        {
+          "yaw": 2.8493530615196487,
+          "pitch": 0.02276950382911025,
+          "title": "Vikas",
+          "text": "SE"
+        },
+        {
+          "yaw": -2.866301353890144,
+          "pitch": 0.007078215462922088,
+          "title": "Neha",
+          "text": "WCD"
+        },
+        {
+          "yaw": -2.502934500525283,
+          "pitch": 0.24223879211014854,
+          "title": "Mayur",
+          "text": "SE"
+        },
+        {
+          "yaw": -2.1297828349405794,
+          "pitch": 0.282443342197487,
+          "title": "Abhishek",
+          "text": "<div>SE</div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
